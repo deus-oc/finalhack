24076d3927abe3e3b17f4ef013a85d80f293efec
deus-oc <anishagrawal.2612@gmail.com>
2019-12-15 17:18:02 +0530

    btn in click not working

--- codeevent.js
+++ codeevent.js
@@ -1,16 +1,23 @@
-// const onclick = async (id_no) => {
-//     const response = await fetch(`https://clist.by/api/v1/contest/?username=amu629&api_key=cfdcf7a003520eb7deadb0d84875aa5c87ce75c2&resource__id=${id_no}&start__gt=2019-12-15T03:07:43&order__by=start&limit=10&utf-8&application/json`);
-//     const responseJson = await response.json();
-//     console.log(responseJson);
-// }
+const onclick = async (id_no) => {
+    console.log(`id is equal to ${id_no}`);
+    const response = await fetch(`https://clist.by/api/v1/contest/?username=amu629&api_key=cfdcf7a003520eb7deadb0d84875aa5c87ce75c2&resource__id=${id_no}&start__gt=2019-12-15T03:07:43&order__by=start&limit=10&utf-8&application/json`);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    const modalbody = document.querySelector(`#modal-body-${id_no}`);
+    console.log(modalbody);
+
+}
 const rowclick = document.querySelector('.row');
 const id = [1,2,12,35,63,73];
-const idname = ['codeforces', 'codechef', 'topcoder', 'google-code-comp', 'hackerrank', 'hackerearth'];
-// const btn = document.querySelector(`#btn-${num}`);
-// btn.addEventListener('click', () => onclick(num));
+const idname = ['Codeforces', 'Codechef', 'Topcoder', 'Google-code-comp', 'Hackerrank', 'Hackerearth'];
 
 const whenClickButton = (e) => {
     console.log(e.target.id);
+    let btn = document.querySelector('#' + e.target.id);
+    let numbers = e.target.id.split('-');
+    console.log(numbers[1]); 
+    console.log(btn);
+    btn.addEventListener('click', () => onclick(numbers[1]));
 }
 
 const fetchallsamplecard = () => {
@@ -27,7 +34,7 @@ const fetchallsamplecard = () => {
                 Get all the upcoming events/rounds on Codeforces .
               </p>
             <!-- Button trigger modal -->
-              <button id = "1" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+              <button id = "btn-${value}" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
               Launch demo modal
               </button>
             <!-- Modal -->
@@ -35,12 +42,12 @@ const fetchallsamplecard = () => {
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="modal-1">Modal title</h5>
+                    <h5 class="modal-title" id="modal-1"><b>${idname[index]}</b></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
+                  <div class="modal-body-${value}">
                     text here is inserted
 
                   </div>
