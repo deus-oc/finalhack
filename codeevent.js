e4bf409bd2e2374d9441477d15def0126a95546f
deus-oc <anishagrawal.2612@gmail.com>
2019-12-15 22:24:58 +0530

    modal not working

--- codeevent.js
+++ codeevent.js
@@ -1,23 +1,23 @@
 const rowclick = document.querySelector('.row');
 const id = [1,2,12,35,63,73];
 const idname = ['Codeforces', 'Codechef', 'Topcoder', 'Google-code-comp', 'Hackerrank', 'Hackerearth'];
+const logo = ['https://cdn.aelieve.com/toptere/Codeforces.jpg', 'https://www.improgrammer.net/wp-content/uploads/2016/01/Codechef-logo.png', 'https://www.theindianwire.com/wp-content/uploads/2018/06/Topcoder-logo.jpg', 'https://cdn-images-1.medium.com/max/1600/1*FHJTiIW9_enNj4RWSQ3NEQ.png', 'http://www.iamwire.com/wp-content/uploads/2014/06/hackerrank_g7yq8.png', 'https://verticalplatform.kr/wp-content/uploads/2014/03/hackerearth-logo.png']
 
-
-const onclick = async (id_no) => {
-    console.log(`id is equal to ${id_no}`);
-    const response = await fetch(`https://clist.by/api/v1/contest/?username=amu629&api_key=cfdcf7a003520eb7deadb0d84875aa5c87ce75c2&resource__id=${id_no}&start__gt=2019-12-15T03:07:43&order__by=start&limit=10&utf-8&application/json`);
+const onclick = async (index_no) => {
+    console.log(`id is equal to ${id[index_no]}`);
+    const response = await fetch(`https://clist.by/api/v1/contest/?username=amu629&api_key=cfdcf7a003520eb7deadb0d84875aa5c87ce75c2&resource__id=${id[index_no]}&start__gt=2019-12-15T03:07:43&order__by=start&limit=10&utf-8&application/json`);
     const responseJson = await response.json();
-    console.log(responseJson); 
-    const modalbody = document.querySelector(`.modal-body-${id_no}`);
-    console.log(modalbody);
-    // const img = document.createElement('div')
-    // img = <div class="card" style="width: 18rem;">
-    //   <img class="card-img-top" src="..." alt="Card image cap">
-    // </div> 
-    // modalbody
-    // const lists = document.createElement('ul');
+    console.log(responseJson.objects); 
+    const modaltitle = document.querySelector(`#modal-title-${index_no}`);
+    console.log(modaltitle);
+    const img = document.createElement('div');
+    img.style.width = '18rem';
+    img.innerHTML = `<img class="card-img-top mx-auto" src="${logo[index_no]}" alt="Card image cap">`;
+    modaltitle.appendChild(img);
 
-
+    // const modalbody = document.querySelector(`#modal-body-${index_no}`);
+    // const ul = document.createElement('ul');
+    document.querySelector(`#real-${index_no}`).click();    
 }
 
 const whenClickButton = async (e) => {
@@ -26,13 +26,14 @@ const whenClickButton = async (e) => {
     console.log(numbers[1]);
     const btn = document.querySelector(`#${e.target.id}`);
     console.log(btn);
-    onclick(numbers[1]);
+    if(numbers[0] !== 'real'){
+      onclick(numbers[1]);
+    }
 }
 
 const fetchallsamplecard = () => {
     id.forEach( (value,index) => {
         
-        // <div class="col-sm-4">
         const cards = document.createElement('div');
         cards.classList.add('col-sm-4');
         cards.innerHTML = `<!-- Sample card 1-->
@@ -44,21 +45,22 @@ const fetchallsamplecard = () => {
               </p>
             <!-- Button trigger modal -->
             <!-- Button = api call-->
-              <button id = "btn-${value}" type="button" class="btn btn-primary">Launch demo modal</button>
-              <button style = "display : none" id = "btn-real-${value}" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">Launch demo modal</button>
+              <button id = "btn-${index}" type="button" class="btn btn-primary">Launch demo modal</button>
+              <button style = "display : none" id = "real-${index}" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">Launch demo modal</button>
             <!-- Modal -->
             <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
                   <div class="modal-header">
-                    <h5 class="modal-title" id="modal-1"><b>${idname[index]}</b></h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                      <div id = "modal-title-${index}"><b>${idname[index]}</b></div>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body-${value}">
-                    text here is inserted
-
+                  <div class="modal-body">
+                    <div id="modal-body-${index}">
+                    </div>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -72,6 +74,6 @@ const fetchallsamplecard = () => {
         </div>
       <!-- Sample card -->`
       rowclick.appendChild(cards);
-    })
+    });
 }
 rowclick.addEventListener('click', whenClickButton);
